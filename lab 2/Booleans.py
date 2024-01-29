f7cc5978811416598732b7e78a421213e94e05bd
AskhatShaikh <156664155+AskhatShaikh@users.noreply.github.com>
2024-01-29 10:53:03 +0600

    Add files via upload

---
+++ lab 2/Booleans.py
@@ -0,0 +1,80 @@
+#Task1
+print(10 > 9)
+print(10 == 9)
+print(10 < 9)
+
+#Task2
+a = 200
+b = 33
+
+if b > a:
+  print("b is greater than a")
+else:
+  print("b is not greater than a")
+
+ #Task3
+  print(bool("Hello"))
+print(bool(15))
+
+#Task4
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+#Task5
+bool("abc")
+bool(123)
+bool(["apple", "cherry", "banana"])
+#Task6
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+#Task7
+class myclass():
+  def __len__(self):
+    return 0
+#Task8
+myobj = myclass()
+print(bool(myobj))
+#Task9
+def myFunction() :
+  return True
+
+print(myFunction())
+#Task10
+def myFunction() :
+  return True
+
+if myFunction():
+  print("YES!")
+else:
+  print("NO!")
+
+  #Task11
+  x = 200
+print(isinstance(x, int))
+#Task12
+print(10 > 9)
+
+True
+
+#Task13
+print(10 == 9)
+
+False
+
+#Task14
+print(10 < 9)
+
+False
+
+#Task15
+print(bool(0))
+
+False
